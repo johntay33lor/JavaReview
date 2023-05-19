e8790122fadf11500b488f1304b9bfc3e5ff9f29
John <johntay33lor@gmail.com>
2023-05-19 18:31:15 -0400

    Initial commit

---
+++ src/warmUp1/sleepIn.java
@@ -0,0 +1,15 @@
+package review;
+
+public class sleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class monkeyTrouble {
+    
+}
